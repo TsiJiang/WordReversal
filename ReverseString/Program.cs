f8a8bb822a6228511ee376465f2d7f17cad6c74a
Xavier Long <xavier.long4@outlook.com>
2020-04-20 13:00:12 -0600

    added actual project

---
+++ ReverseString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringReversal s1 = new StringReversal();
+            Console.Write("Enter a word: ");
+            s1.SetWord(Console.ReadLine());
+            Console.WriteLine(s1.GetWord());
+            Console.WriteLine(s1.GetDrow());
+        }
+    }
+}
---
+++ ReverseString/StringReversal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class StringReversal
+    {
+        private string ForwardWord;
+        private string BackwardsWord;
+
+        public void SetWord(string word) 
+        {
+            ForwardWord = word;
+            word = string.Concat(word);
+            BackwardsWord = "";
+            for (int i = word.Length - 1; i >= 0; i--) 
+            {
+                BackwardsWord += word[i];
+            }
+        }
+        public string GetWord() 
+        {
+            return ForwardWord;
+        }
+        public string GetDrow() 
+        {
+            return BackwardsWord;
+        }
+    }
+}
